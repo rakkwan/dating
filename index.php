621750916729074b6d8345b9801803ff40ca17dc
Jittima Goodrich <jgoodrich4@mail.greenriver.edu>
2019-04-10 20:48:59 -0400

    Add composer.json and index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jrakk
+ * Date: 4/8/2019
+ * Time: 2:16 PM
+ */
+
+    // Turn on error reporting
+    ini_set('display_error', 1);
+    error_reporting(E_ALL);
+
+    //require autoload file
+    require_once ('vendor/autoload.php');
+
+    // create an instance of the base class
+    $f3 = Base::instance();
+
+    // Turn on Fat-free error reporting
+    $f3->set('DEBUG', 3);
+
+    // define a default route
+    $f3->route('GET /', function()
+    {
+        $view = new Template();
+        echo $view->render('views/home.html');
+    });
+
+    // Run Fat-Free
+    $f3->run();
