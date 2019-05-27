0352fbd5d01c67283fe23543d900774e527a490b
Jittima Goodrich <jgoodrich4@mail.greenriver.edu>
2019-05-26 23:06:46 -0400

    Made database function

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: jrakk
+ * User: Jittima Goodrich
  * Date: 5/26/2019
  * Time: 11:06 AM
  */
@@ -42,26 +42,46 @@ CREATE TABLE memberinterest
 );
  */
 require '/home/jgoodric/config-student.php';
+
+/**
+ * Class Database connect to database
+ * @author Jittima Goodrich
+ * @version 1.0
+ * @copyright 2019
+ */
 class Database
 {
     private $_dbh;
 
+    /**
+     * Database constructor.
+     * @return void
+     */
     function __construct()
     {
         $this->connect();
     }
 
+    /**
+     * Connection to database
+     * @return void
+     */
     function connect()
     {
         try {
             // Instantiate a db object
             $this->_dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-            return $this->_dbh;
+            //return $this->_dbh;
         } catch (PDOException $e) {
             $this->_dbh = $e->getMessage();
         }
     }
 
+    /**
+     * Insert a member into database
+     * @param $member
+     * @return void
+     */
     function insertMember($member)
     {
         if (isset($this->_dbh)) {
@@ -102,7 +122,100 @@ class Database
 
             //Execute the statement
             $statement->execute();
-        }
 
+            // grab id
+            $id = $this->_dbh->lastInsertId();
+
+            //check if Premium member to insert
+            if($member instanceof PremiumMember) {
+                $indoor = $member->getInDoorInterests();
+                $outdoor = $member->getOutDoorInterests();
+                if(isset($indoor))
+                {
+                    $this->insertInterest($indoor, $id);
+                }
+                if(isset($outdoor))
+                {
+                    $this->insertInterest($outdoor, $id);
+                }
+            }
+        }
+    }
+
+    /**
+     * Insert interests into database
+     * @param $interest
+     * @param $id
+     */
+    private function insertInterest($interest, $id)
+    {
+        $sql = "INSERT INTO memberinterest(member_id, interest_id)
+                VALUES(:member, :interest)";
+        $statement = $this->_dbh->prepare($sql);
+
+        foreach ($interest as $value) {
+            // bind interest id and member id
+            $statement->bindParam(":member", $id, PDO::PARAM_INT);
+            $statement->bindParam(":interest", $this->getID($value), PDO::PARAM_INT);
+
+            //Execute the statement
+            $statement->execute();
+        }
+    }
+
+    /**
+     * check interest id
+     * @param $interest_id id of interest
+     * @return mixed Int interest
+     */
+    private function getID($interest_id)
+    {
+        $sql = "SELECT interest_id FROM interest WHERE interest_id = :interest_id";
+        $statement = $this->_dbh->prepare($sql);
+
+        $statement->bindParam(':interest', $interest_id, PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_NUM);
+    }
+
+    /**
+     * Retrieves members from the database
+     * @return String from database
+     */
+    function getMembers()
+    {
+        $sql = "SELECT * FROM member ORDER BY lname";
+        $statement = $this->_dbh->prepare($sql);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Retrieves a member from the database
+     * @param $member_id - the member id
+     * @return String from the database
+     */
+    function getMember($member_id)
+    {
+        $sql ="SELECT * FROM member WHERE member_id = :member_id";
+        $statement = $this->_dbh->prepare($sql);
+        $statement->bindParam(":member_id", $member_id, PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * String of interest
+     * @param $member_id id of the member
+     * @return String interest
+     */
+    function getInterests($member_id)
+    {
+        $sql = "SELECT i.interest FROM memberinterest AS mi INNER JOIN interest AS i 
+                ON mi.interest_id = i.interest_id WHERE mi.member_id = :member_id";
+        $statement = $this->_dbh->prepare($sql);
+        $statement->bindParam(':member_id', $member_id, PDO::PARAM_STR);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 }
